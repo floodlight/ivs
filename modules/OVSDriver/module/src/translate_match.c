b9544c7239b97716433ab0eec7cf251e38382634
Rich Lane <rlane@bigswitch.com>
2014-01-17 15:11:14 -0800

    OVSDriver: fix CFR in_ports for reserved ports
    aim_imin accepts signed integers, so large port numbers like OFPP_TABLE are
    interpreted by it as small negative numbers.

--- modules/OVSDriver/module/src/translate_match.c
+++ modules/OVSDriver/module/src/translate_match.c
@@ -219,7 +219,8 @@ ind_ovs_key_to_cfr(const struct ind_ovs_parsed_key *pkey,
 
     /* Set a bit in the in_ports bitmap */
     {
-        uint32_t idx = aim_imin(IVS_MAX_BITMAP_IN_PORT, pkey->in_port);
+        uint32_t idx = IVS_MAX_BITMAP_IN_PORT < pkey->in_port ?
+            IVS_MAX_BITMAP_IN_PORT : pkey->in_port;
         uint32_t word = IVS_MAX_BITMAP_IN_PORT/32 - idx/32;
         uint32_t bit = idx % 32;
         memset(cfr->in_ports, 0, sizeof(cfr->in_ports));
