86902590d350de310b3f7a07a2723bbc07997dc6
Rich Lane <rlane@bigswitch.com>
2014-03-19 17:11:35 -0700

    OVSDriver: parse of_instruction_bsn_packet_of_death

--- modules/ivs/module/inc/ivs/ivs.h
+++ modules/ivs/module/inc/ivs/ivs.h
@@ -71,6 +71,7 @@ struct ind_ovs_flow_effects {
     unsigned disable_split_horizon_check : 1;
     unsigned permit : 1;
     unsigned deny : 1;
+    unsigned packet_of_death : 1;
 };
 
 struct ind_ovs_flow_stats {
