935c58e30c0b804e6d0993e4601f73e0b71c3272
Rich Lane <rlane@bigswitch.com>
2014-11-04 13:57:10 -0800

    pipeline_lua: skeleton
    Registers a new no-op "lua" pipeline.

---
+++ modules/pipeline_lua/module/src/pipeline_lua.c
@@ -0,0 +1,63 @@
+/****************************************************************
+ *
+ *        Copyright 2014, Big Switch Networks, Inc.
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *
+ ****************************************************************/
+
+#include <pipeline/pipeline.h>
+#include <stdlib.h>
+
+#include <ivs/ivs.h>
+#include <loci/loci.h>
+#include <OVSDriver/ovsdriver.h>
+#include <indigo/indigo.h>
+#include <indigo/of_state_manager.h>
+
+#define AIM_LOG_MODULE_NAME pipeline_lua
+#include <AIM/aim_log.h>
+
+AIM_LOG_STRUCT_DEFINE(AIM_LOG_OPTIONS_DEFAULT, AIM_LOG_BITS_DEFAULT, NULL, 0);
+
+static void
+pipeline_lua_init(const char *name)
+{
+}
+
+static void
+pipeline_lua_finish(void)
+{
+}
+
+indigo_error_t
+pipeline_lua_process(struct ind_ovs_parsed_key *key,
+                     struct xbuf *stats,
+                     struct action_context *actx)
+{
+    return INDIGO_ERROR_NONE;
+}
+
+static struct pipeline_ops pipeline_lua_ops = {
+    .init = pipeline_lua_init,
+    .finish = pipeline_lua_finish,
+    .process = pipeline_lua_process,
+};
+
+void
+__pipeline_lua_module_init__(void)
+{
+    AIM_LOG_STRUCT_REGISTER();
+    pipeline_register("lua", &pipeline_lua_ops);
+}
