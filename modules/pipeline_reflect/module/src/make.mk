e93e936d1e7fce0beb73bc4e3f3b89fc6aa4c5c3
Rich Lane <rlane@bigswitch.com>
2014-11-12 16:45:15 -0800

    add a dummy pipeline for performance testing
    This pipeline just echoes packets back out the port they came in on. It can
    optionally delay the packets by a number of nanoseconds given in an environment
    variable.

--- modules/pipeline_reflect/module/src/make.mk
+++ modules/pipeline_reflect/module/src/make.mk
@@ -17,16 +17,6 @@
 #
 ################################################################
 
-BASEDIR := $(dir $(lastword $(MAKEFILE_LIST)))
-OVSDriver_BASEDIR := $(BASEDIR)/OVSDriver
-flowtable_BASEDIR := $(BASEDIR)/flowtable
-l2table_BASEDIR := $(BASEDIR)/l2table
-luajit_BASEDIR := $(BASEDIR)/luajit
-xbuf_BASEDIR := $(BASEDIR)/xbuf
-pipeline_BASEDIR := $(BASEDIR)/pipeline
-pipeline_standard_BASEDIR := $(BASEDIR)/pipeline_standard
-ivs_common_BASEDIR := $(BASEDIR)/ivs
-tcam_BASEDIR := $(BASEDIR)/tcam
-action_BASEDIR := $(BASEDIR)/action
-stats_BASEDIR := $(BASEDIR)/stats
-pipeline_reflect_BASEDIR := $(BASEDIR)/pipeline_reflect
+LIBRARY := pipeline_reflect
+$(LIBRARY)_SUBDIR := $(dir $(lastword $(MAKEFILE_LIST)))
+include $(BUILDER)/lib.mk
