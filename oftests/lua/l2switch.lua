c54559c53fc980e64c1c5329b76fb3b305e6ed13
Rich Lane <rlane@bigswitch.com>
2015-04-27 17:56:37 -0700

    l2switch: combine l2 and vlan tables into a single endpoint table
    The L2 and VLAN tables used for forwarding are computed from a single table
    programmed by the controller.

--- oftests/lua/l2switch.lua
+++ oftests/lua/l2switch.lua
@@ -19,8 +19,6 @@
 -- OFTests against.
 --
 -- TODO send packet-in instead of dropping
--- TODO use a single "endpoint" OpenFlow table
--- TODO remove limitation of 32 ports
 -- TODO send LLDPs to controller
 
 local fields = fields
@@ -28,67 +26,70 @@ local bit_check, flood
 local xdr = require("l2switch_xdr")
 
 local l2_table = hashtable.create({ "vlan", "mac_hi", "mac_lo" }, { "port", "stats" })
-local l2_stats = {}
 
-register_table("l2", {
-    parse_key=xdr.read_l2_key,
-    parse_value=xdr.read_l2_value,
+local vlan_table = {} -- vlan -> { port -> refcount }
+for vlan = 0, 4095 do
+    vlan_table[vlan] = {}
+end
+
+local function vlan_add_member(vlan, port)
+    vlan_entry = vlan_table[vlan]
+    vlan_entry[port] = (vlan_entry[port] or 0) + 1
+end
+
+local function vlan_remove_member(vlan, port)
+    vlan_entry = vlan_table[vlan]
+    local refcount = vlan_entry[port]
+    if refcount > 1 then
+        vlan_entry[port] = refcount - 1
+    else
+        vlan_entry[port] = nil
+    end
+end
+
+local endpoints = {} -- cookie -> { stats, port }
+
+register_table("endpoint", {
+    parse_key=xdr.read_endpoint_key,
+    parse_value=xdr.read_endpoint_value,
 
     add=function(k, v, cookie)
-        log("l2_add %p: vlan=%u mac=%04x%08x -> port %u", cookie, k.vlan, k.mac_hi, k.mac_lo, v.port)
-        l2_stats[cookie] = stats.alloc()
-        l2_table:insert(k, { port=v.port, stats=l2_stats[cookie] })
+        log("endpoint add %p: vlan=%u mac=%04x%08x -> port %u", cookie, k.vlan, k.mac_hi, k.mac_lo, v.port)
+        local s = stats.alloc()
+        l2_table:insert(k, { port=v.port, stats=s })
+        vlan_add_member(k.vlan, v.port)
+        endpoints[cookie] = { port=v.port, stats=s }
     end,
 
     modify=function(k, v, cookie)
-        log("l2_modify %p: vlan=%u mac=%04x%08x -> port %u", cookie, k.vlan, k.mac_hi, k.mac_lo, v.port)
-        l2_table:insert(k, { port=v.port, stats=l2_stats[cookie] })
+        log("endpoint modify %p: vlan=%u mac=%04x%08x -> port %u", cookie, k.vlan, k.mac_hi, k.mac_lo, v.port)
+        local e = endpoints[cookie]
+        l2_table:insert(k, { port=v.port, stats=e.stats })
+        vlan_remove_member(k.vlan, e.port) -- remove old port from VLAN
+        vlan_add_member(k.vlan, v.port) -- add new port to VLAN
     end,
 
     delete=function(k, cookie)
-        log("l2_delete %p: vlan=%u mac=%04x%08x", cookie, k.vlan, k.mac_hi, k.mac_lo)
-        stats.free(l2_stats[cookie])
-        l2_stats[cookie] = nil
+        log("endpoint delete %p: vlan=%u mac=%04x%08x", cookie, k.vlan, k.mac_hi, k.mac_lo)
+        local e = endpoints[cookie]
         l2_table:remove(k)
+        vlan_remove_member(k.vlan, e.port)
+        stats.free(e.stats)
+        endpoints[cookie] = nil
     end,
 
     get_stats=function(k, writer, cookie)
-        log("l2_get_stats %p: vlan=%u mac=%04x%08x", cookie, k.vlan, k.mac_hi, k.mac_lo)
-        local packets, bytes = stats.get(l2_stats[cookie])
-        xdr.write_l2_stats(writer, { packets=packets, bytes=bytes })
+        log("endpoint get_stats %p: vlan=%u mac=%04x%08x", cookie, k.vlan, k.mac_hi, k.mac_lo)
+        local e = endpoints[cookie]
+        local packets, bytes = stats.get(e.stats)
+        xdr.write_endpoint_stats(writer, { packets=packets, bytes=bytes })
     end
-})
-
-local vlan_table = hashtable.create({ "vlan" }, { "port_bitmap" })
-
-register_table("vlan", {
-    parse_key=xdr.read_vlan_key,
-    parse_value=xdr.read_vlan_value,
-
-    add=function(k, v, cookie)
-        log("vlan_add %p: vlan=%u -> port_bitmap %08x", cookie, k.vlan, v.port_bitmap)
-        vlan_table:insert(k, v)
-    end,
-
-    modify=function(k, v, cookie)
-        log("vlan_modify %p: vlan=%u -> port_bitmap %08x", cookie, k.vlan, v.port_bitmap)
-        vlan_table:insert(k, v)
-    end,
-
-    delete=function(k, cookie)
-        log("vlan_delete %p: vlan=%u", cookie, k.vlan)
-        vlan_table:remove(k)
-    end,
 })
 
 function ingress()
-    local vlan_entry = vlan_table:lookup({ vlan=fields.vlan_vid })
-    if not vlan_entry then
-        trace("VLAN lookup failure, dropping")
-        return
-    end
+    local vlan_entry = vlan_table[fields.vlan_vid]
 
-    if not bit_check(vlan_entry.port_bitmap, fields.in_port) then
+    if not vlan_entry[fields.in_port] then
         trace("Port %u not allowed on VLAN %u, dropping", fields.in_port, fields.vlan_vid)
         return
     end
@@ -123,13 +124,7 @@ function ingress()
 end
 
 function flood(vlan_entry)
-    for port = 0, 31 do
-        if bit_check(vlan_entry.port_bitmap, port) then
-            output(port)
-        end
+    for port in pairs(vlan_entry) do
+        output(port)
     end
-end
-
-function bit_check(bitmap, index)
-    return bit.band(bitmap, bit.lshift(1, index)) ~= 0
 end
